5b7aa6eeb4ffe0cfd8783772a4c457f5d37b79ed
Rosialdo <79479462+Rosialdo@users.noreply.github.com>
2023-05-21 11:38:08 -0400

    Atividade 3 mochila 0-1 inicial

---
+++ Atividade3/Mochilha_0-1/README.md
@@ -0,0 +1,5 @@
+# Repositorio criado para a atividade 3 de Inteligencia Artificial da universidade Federal de Roraima (UFRR) pelo semestre 2023.1
+
+##  Atividade sobre Algoritimos Geneticos
+
+### Problema da mochila 0-1
---
+++ Atividade3/Mochilha_0-1/mochila_0-1.py
@@ -0,0 +1,57 @@
+from genetic2020 import *
+
+                  #[peso,valor]
+pesos_e_valores = [[3, 266], 
+                   [13, 442], 
+                   [10, 671], 
+                   [9, 526], 
+                   [2, 10], 
+                   [7, 388], 
+                   [1, 245], 
+                   [8, 210 ], 
+                   [8, 145],
+                   [2, 126 ],
+                   [9, 322]]
+peso_maximo = 35
+n_de_cromossomos = 80
+geracoes = 150
+n_de_itens = len(pesos_e_valores) #Analogo aos pesos e valores
+
+
+#EXECUCAO DOS PROCEDIMENTOS
+populacao = population(n_de_cromossomos, n_de_itens)
+historico_de_fitness = [media_fitness(populacao, peso_maximo, pesos_e_valores)]
+for i in range(geracoes):
+    populacao = evolve(populacao, peso_maximo, pesos_e_valores, n_de_cromossomos)
+    historico_de_fitness.append(media_fitness(populacao, peso_maximo, pesos_e_valores))
+
+#PRINTS DO TERMINAL
+for indice,dados in enumerate(historico_de_fitness):
+   print ("Geracao: ", indice," | Media de valor na mochila: ", dados)
+
+print("\nPeso máximo:",peso_maximo,"g\n\nItens disponíveis:")
+for indice,i in enumerate(pesos_e_valores):
+    print("Item ",indice+1,": ",i[0],"g | R$",i[1])
+    
+# Exemplos de boas soluções
+print("\nExemplos de boas soluções: ")
+for i in range(5):
+    individuo = populacao[i]
+    peso_total, valor_total = 0, 0
+    for indice, valor in enumerate(individuo):
+        peso_total += (individuo[indice] * pesos_e_valores[indice][0])
+        valor_total += (individuo[indice] * pesos_e_valores[indice][1])
+    print("Solução", i + 1, ":")
+    print("   - Peso: ", peso_total, "g")
+    print("   - Valor: R$", valor_total)
+
+
+
+#GERADOR DE GRAFICO
+from matplotlib import pyplot as plt
+plt.plot(range(len(historico_de_fitness)), historico_de_fitness)
+plt.grid(True, zorder=0)
+plt.title("Problema da mochila")
+plt.xlabel("Geracao")
+plt.ylabel("Valor medio da mochila")
+plt.show()
